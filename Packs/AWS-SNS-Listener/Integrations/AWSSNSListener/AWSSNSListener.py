c9a06b1e12141482c72d6ce565d1efe78998e670
Danny_Fried <dfried@paloaltonetworks.com>
2023-11-27 18:25:58 +0200

    temp changes.

--- Packs/AWS-SNS-Listener/Integrations/AWSSNSListener/AWSSNSListener.py
+++ Packs/AWS-SNS-Listener/Integrations/AWSSNSListener/AWSSNSListener.py
@@ -2,13 +2,13 @@ import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 from tempfile import NamedTemporaryFile
 from traceback import format_exc
-
+from collections import deque
 import uvicorn
 from secrets import compare_digest
 from fastapi import Depends, FastAPI, Request, Response, status
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from fastapi.security.api_key import APIKey, APIKeyHeader
-
+from pydantic import BaseModel
 from CommonServerUserPython import *
 
 sample_events_to_store = deque(maxlen=20)  # type: ignore[var-annotated]
@@ -17,81 +17,76 @@ app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
 basic_auth = HTTPBasic(auto_error=False)
 token_auth = APIKeyHeader(auto_error=False, name='Authorization')
 
+
 class Incident(BaseModel):
     name: Optional[str] = None
     type: Optional[str] = None
     occurred: Optional[str] = None
     raw_json: Optional[Dict] = None
 
-@app.post('/')
+
+@app.post('/sns')
 async def handle_post(
         incident: Incident,
         request: Request,
         credentials: HTTPBasicCredentials = Depends(basic_auth),
         token: APIKey = Depends(token_auth)
 ):
+    demisto.error("Got AWS SNS request")
     header_name = None
     request_headers = dict(request.headers)
 
     credentials_param = demisto.params().get('credentials')
 
-    if credentials_param and (username := credentials_param.get('identifier')):
-        password = credentials_param.get('password', '')
-        auth_failed = False
-        if username.startswith('_header'):
-            header_name = username.split(':')[1]
-            token_auth.model.name = header_name
-            if not token or not compare_digest(token, password):
-                auth_failed = True
-        elif (not credentials) or (not (compare_digest(credentials.username, username)
-                                   and compare_digest(credentials.password, password))):
-            auth_failed = True
-        if auth_failed:
-            secret_header = (header_name or 'Authorization').lower()
-            if secret_header in request_headers:
-                request_headers[secret_header] = '***'
-            demisto.debug(f'Authorization failed - request headers {request_headers}')
-            return Response(status_code=status.HTTP_401_UNAUTHORIZED, content='Authorization failed.')
+    # return demisto.createIncidents([incident])
 
-    secret_header = (header_name or 'Authorization').lower()
-    request_headers.pop(secret_header, None)
 
-    raw_json = incident.raw_json or await request.json()
-    raw_json['headers'] = request_headers
+# def handle_external_alert_aws(ac, w, r):
+#     aws_request = get_request_body(r)
+#     if aws_request.type == "SubscriptionConfirmation":
+#         demisto.info("Got AWS SNS subscription confirmation request")
+#         req = requests.get(aws_request.subscribe_url)
+#         if req.status_code < 200 or req.status_code >= 300:
+#             demisto.error(f"Failed handling AWS SNS request, got response: {req.status_code}")
+#             write_error(w, ErrAWSSNSSubscriptionRequest, req.text, r)
+#             return
+#         demisto.info(f"AWS SNS subscription has sent successfully, returned status code: {req.status_code}")
+#     elif aws_request.type == "Notification":
+#         req = CreateIncidentRequest(incident=Incident())
+#         req.incident.name = aws_request.subject
+#         req.incident.raw_json_data = aws_request.message
+#         req.create_investigation = conf.get_config_bool_or_provided("aws.incidents.api.investigate", True)
+#         classifier = conf.get_config_string_or_provided(conf.ExternalServicesClassifierPrefix+domain.AWSDefaultMappingID, "")
+#         mapper = conf.get_config_string_or_provided(conf.ExternalServicesMapperPrefix+domain.AWSDefaultMappingID, "")
+#         ac.handle_external_alert(w, r, req, classifier, mapper)
+#     else:
+#         demisto.error("Failed handling AWS SNS request")
+#         write_error(w, ErrAWSSNSSubscriptionRequest, "Invalid request type", r)
 
-    incident = {
-        'name': incident.name or 'Generic webhook triggered incident',
-        'type': incident.type or demisto.params().get('incidentType'),
-        'occurred': incident.occurred,
-        'rawJSON': json.dumps(raw_json)
-    }
-
-    if demisto.params().get('store_samples'):
-        try:
-            sample_events_to_store.append(incident)
-            integration_context = get_integration_context()
-            sample_events = deque(json.loads(integration_context.get('sample_events', '[]')), maxlen=20)
-            sample_events += sample_events_to_store
-            integration_context['sample_events'] = list(sample_events)
-            set_to_integration_context_with_retries(integration_context)
-        except Exception as e:
-            demisto.error(f'Failed storing sample events - {e}')
-
-    return demisto.createIncidents([incident])
 
 
 ''' MAIN FUNCTION '''
 
 
-def main() -> None:
+def main():
     demisto.debug(f'Command being called is {demisto.command()}')
+    params = demisto.params()
+    aws_default_region = params.get('defaultRegion')
+    aws_role_arn = params.get('roleArn')
+    aws_role_session_name = params.get('roleSessionName')
+    aws_role_policy = None
+    aws_access_key_id = params.get('access_key')
+    aws_secret_access_key = params.get('secret_key')
+    verify_certificate = not params.get('insecure', False)
+    # validate_params(aws_default_region, aws_role_arn, aws_role_session_name, aws_access_key_id,
+    #                     aws_secret_access_key)
     try:
         try:
             port = int(demisto.params().get('longRunningPort'))
         except ValueError as e:
             raise ValueError(f'Invalid listen port - {e}')
         if demisto.command() == 'test-module':
-            return_results('ok')
+            return "ok"
         # elif demisto.command() == 'fetch-incidents':
         #     fetch_samples()
         elif demisto.command() == 'long-running-execution':
@@ -104,22 +99,22 @@ def main() -> None:
                 try:
                     ssl_args = dict()
 
-                    if certificate and private_key:
-                        certificate_file = NamedTemporaryFile(delete=False)
-                        certificate_path = certificate_file.name
-                        certificate_file.write(bytes(certificate, 'utf-8'))
-                        certificate_file.close()
-                        ssl_args['ssl_certfile'] = certificate_path
+                    # if certificate and private_key:
+                    #     certificate_file = NamedTemporaryFile(delete=False)
+                    #     certificate_path = certificate_file.name
+                    #     certificate_file.write(bytes(certificate, 'utf-8'))
+                    #     certificate_file.close()
+                    #     ssl_args['ssl_certfile'] = certificate_path
 
-                        private_key_file = NamedTemporaryFile(delete=False)
-                        private_key_path = private_key_file.name
-                        private_key_file.write(bytes(private_key, 'utf-8'))
-                        private_key_file.close()
-                        ssl_args['ssl_keyfile'] = private_key_path
+                    #     private_key_file = NamedTemporaryFile(delete=False)
+                    #     private_key_path = private_key_file.name
+                    #     private_key_file.write(bytes(private_key, 'utf-8'))
+                    #     private_key_file.close()
+                    #     ssl_args['ssl_keyfile'] = private_key_path
 
-                        demisto.debug('Starting HTTPS Server')
-                    else:
-                        demisto.debug('Starting HTTP Server')
+                    #     demisto.debug('Starting HTTPS Server')
+                    # else:
+                    #     demisto.debug('Starting HTTP Server')
 
                     integration_logger = IntegrationLogger()
                     integration_logger.buffering = False
